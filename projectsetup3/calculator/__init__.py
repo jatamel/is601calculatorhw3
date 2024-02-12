11d545417d51e3691cf86480fd3cc10eadd25253
jatamel <jm2449@njit.edu>
2024-02-11 22:42:31 -0500

    added a multi and div function

--- projectsetup3/calculator/__init__.py
+++ projectsetup3/calculator/__init__.py
@@ -7,3 +7,11 @@ def add(a,b):
 def subtract(a,b):
     '''so this is how we subtract..'''
     return a - b
+
+def multiply(a,b):
+    "multiply two varibles"
+    return a * b
+
+def divide(a,b):
+    "divide two varibles"
+    return b / a
--- projectsetup3/tests/test_calculator.py
+++ projectsetup3/tests/test_calculator.py
@@ -2,9 +2,17 @@
 from calculator import add, subtract
 
 def test_addition():
-    '''meep porp this is a test addition function'''
+    "test that addition function works"
     assert add(2,2) == 4
 
 def test_subrtaction():
-    '''meep porp this is a test subtract function'''
+    "test that subtraction function works"
     assert subtract(2,2) == 0
+
+def test_multiply():
+    "test that multiplication function works"
+    assert multiply(2,3) == 6
+
+def test_divide():
+    "test that multiplication function works"
+    assert divide(3,3) == 1

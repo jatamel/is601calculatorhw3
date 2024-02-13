c7840ef0c1bb3ec38172fa0779d7ecf66fe40387
jatamel <jm2449@njit.edu>
2024-02-12 22:01:22 -0500

    placed operation functions in here

---
+++ projectsetup3/calculator/operations.py
@@ -0,0 +1,17 @@
+'''this is a calculator'''
+
+def add(a,b):
+    '''so i need a docstring here too ig..'''
+    return a + b
+
+def subtract(a,b):
+    '''so this is how we subtract..'''
+    return a - b
+
+def multiply(a,b):
+    "multiply two varibles"
+    return a * b
+
+def divide(a,b):
+    "divide two varibles"
+    return a / b

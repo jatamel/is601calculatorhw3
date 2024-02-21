b050eaa0b6d72b3b13d3a16e32080bab9c4b6690
jatamel <jm2449@njit.edu>
2024-02-21 00:16:22 -0500

    idk why vsc doesn't like the import pytest line but i do so it stays

--- projectsetup3/tests/test_operations.py
+++ projectsetup3/tests/test_operations.py
@@ -1,12 +1,11 @@
-import pytest
 from decimal import Decimal
+import pytest
 from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
-
 def test_operation(a, b, operation, expected):
     '''Testing various operations'''
-    calculation = Calculation(a, b, operation)
+    calculation = Calculation.create(a, b, operation)
     assert calculation.perform() == expected, f"{operation.__name__} operation failed"
 
 # Keeping the divide by zero test as is since it tests a specific case
